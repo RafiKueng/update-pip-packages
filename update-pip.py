f73afc89bcdd3e72013871b5055dae2eb779318e
Rafael Küng <rafik@physik.uzh.ch>
2014-11-24 14:07:18 +0100

    added commandline args
    But stopped working on it and just use pip-tools, as mentioned in blog from orginal
    author a few posts further up...

--- update-pip.py
+++ update-pip.py
@@ -8,14 +8,25 @@ that you have installed globally with pip
 http://pythonadventures.wordpress.com/2013/05/22/update-all-pip-packages/
 
 Jabba Laci, 2013--2014 (jabba.laci@gmail.com)
+
+Modified to accept arguments
+Rafael Kueng, 2014-11-24 (rafi.kueng@gmx.ch)
 """
 
 from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 import os
-
 import pip
+import argparse
+
+parser = argparse.ArgumentParser()
+#parser.add_argument("--verbosity", help="increase output verbosity")
+group = parser.add_mutually_exclusive_group()
+group.add_argument("-d", "--dry",    help="do a dryrun (print commands to stdout instead of running)", action="store_true")
+group.add_argument("-l", "--list",   help="list packages that will be updated", action="store_true")
+group.add_argument("-u", "--update", help="update packages (asks for sudo if not in venv)", action="store_true")
+args = parser.parse_args()
 
 
 dists = []
@@ -25,7 +36,14 @@ for dist in pip.get_installed_distributions():
 dists = sorted(dists, key=lambda s: s.lower())
 dists.insert(0, 'pip')  # let 'pip' be the first
 
-for dist_name in dists:
-    cmd = "sudo pip install {0} -U".format(dist_name)
-    print('#', cmd)
-    os.system(cmd)
+if args.dry or args.list or args.update:
+    for dist_name in dists:
+        if args.list:
+            print(dist_name)
+            continue
+        cmd = "sudo pip install {0} -U".format(dist_name)
+        print('#', cmd)
+        if args.update:
+            os.system(cmd)
+else:
+    parser.print_help()
